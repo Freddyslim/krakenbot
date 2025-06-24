9ab05c0f4898ea1fa03ab3e984718c612dcd2e36
Freddyslim <57746478+Freddyslim@users.noreply.github.com>
2025-06-24 21:41:17 +0200

    Refactor Kraken API client

--- lib/kraken_api.py
+++ lib/kraken_api.py
@@ -1,419 +1,136 @@
-
-"""Provide several sample math calculations.
-
-This module allows the user to make mathematical calculations.
-
-The module contains the following functions:
-
-- `add(a, b)` - Returns the sum of two numbers.
-- `subtract(a, b)` - Returns the difference of two numbers.
-- `multiply(a, b)` - Returns the product of two numbers.
-o numbers.
-"""
-
-# imports
-import requests
-import hmac
-import hashlib
 import base64
-import urllib.parse
-import json
+import hashlib
+import hmac
 import time
+import urllib.parse
+from typing import Any, Dict, Optional
+import functools
+
+import requests
+
 import lib
 
-# constants
-api_url = "https://api.kraken.com"
+API_URL = "https://api.kraken.com"
+
+# Known public and private method names as defined by Kraken's REST API.
+PUBLIC_METHODS = {
+    "Time",
+    "SystemStatus",
+    "Assets",
+    "AssetPairs",
+    "Ticker",
+    "OHLC",
+    "Depth",
+    "Trades",
+    "Spread",
+}
+
+PRIVATE_METHODS = {
+    "Balance",
+    "TradeBalance",
+    "OpenOrders",
+    "ClosedOrders",
+    "QueryOrders",
+    "TradesHistory",
+    "QueryTrades",
+    "OpenPositions",
+    "Ledgers",
+    "QueryLedgers",
+    "TradeVolume",
+    "AddOrder",
+    "CancelOrder",
+    "AddExport",
+    "GetExportReportStatus",
+    "RetrieveExport",
+    "RemoveExport",
+    "Withdraw",
+}
 
 
-#######################################################################
-############################# functions ###############################
-#######################################################################
-############################## general ################################
-
-# get api from key.file
-def get_api_pair(data, api_name):
-    """_summary_
-
-    Args:
-        data (_type_): _description_
-        api_name (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
+def get_api_pair(data: Dict[str, Dict[str, str]], api_name: str):
+    """Return API key and secret for the given API profile."""
     for key in data:
-        if data[key]['name'] == api_name:
-            return data[key]['api_key'], data[key]['api_sec']
-
-# get kraken Signature
-def get_kraken_signature(urlpath, data, secret):
-    """_summary_
-
-    Args:
-        urlpath (_type_): _description_
-        data (_type_): _description_
-        secret (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    postdata = urllib.parse.urlencode(data)
-    encoded = (str(data['nonce'])+ postdata).encode()
-    message = urlpath.encode() + hashlib.sha256(encoded).digest()
-
-    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
-    sigdigest = base64.b64encode(mac.digest())
-    return sigdigest.decode()
-
-# attaches auth headers and returns results of a POST request
-def kraken_request(uri_path, data, api_key, api_sec, api_url):
-    """_summary_
-
-    Args:
-        uri_path (_type_): _description_
-        data (_type_): _description_
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-        api_url (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    headers = {}
-    headers['API-Key'] = api_key
-    # get_kraken_signature() as defined in the 'Authentication' section
-    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)             
-    req = requests.post((api_url + uri_path), headers=headers, data=data)
-    return req
+        if data[key].get("name") == api_name:
+            return data[key].get("api_key"), data[key].get("api_sec")
+    return None, None
 
 
-    
-############################## general ################################
-#######################################################################
-########################### public endpoint ###########################
+class KrakenAPI:
+    """Minimal Kraken REST API client."""
 
-def ticker():
-    """_summary_
+    def __init__(self, api_key: Optional[str] = None, api_sec: Optional[str] = None, api_url: str = API_URL):
+        self.api_key = api_key
+        self.api_sec = api_sec
+        self.api_url = api_url.rstrip("/")
+        self.session = requests.Session()
 
-    Returns:
-        _type_: _description_
-    """
-    resp = requests.get('https://api.kraken.com/0/public/Ticker?pair=XMREUR')
-    # print(resp.json())
-    return resp.json()
+    # ------------------------------------------------------------------
+    # Internal helpers
+    # ------------------------------------------------------------------
+    def _sign(self, urlpath: str, data: Dict[str, Any]) -> str:
+        postdata = urllib.parse.urlencode(data)
+        encoded = (str(data["nonce"]) + postdata).encode()
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
+        mac = hmac.new(base64.b64decode(self.api_sec), message, hashlib.sha512)
+        return base64.b64encode(mac.digest()).decode()
 
-########################### public endpoint ###########################
-#######################################################################
-############################## privat endpoint ########################
+    def _request(self, method: str, urlpath: str, data: Optional[Dict[str, Any]] = None, *, private: bool = False):
+        if data is None:
+            data = {}
+        if private:
+            if not self.api_key or not self.api_sec:
+                raise ValueError("API key and secret required for private endpoints")
+            data["nonce"] = int(time.time() * 1000)
+            headers = {
+                "API-Key": self.api_key,
+                "API-Sign": self._sign(urlpath, data),
+            }
+            resp = self.session.post(self.api_url + urlpath, headers=headers, data=data)
+        else:
+            resp = self.session.get(self.api_url + urlpath, params=data)
+        resp.raise_for_status()
+        return resp.json()
 
-# get account balances
-def get_acc_balance(api_key, api_sec):
-    """_summary_
+    # ------------------------------------------------------------------
+    # Public and private query helpers
+    # ------------------------------------------------------------------
+    def public(self, method: str, params: Optional[Dict[str, Any]] = None):
+        return self._request("GET", f"/0/public/{method}", params, private=False)
 
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/Balance', {
-        "nonce": str(int(1000*time.time()))
-    }, api_key, api_sec, api_url)
-    # print(resp.json())
-    ### to catch only the result part
-    data = resp.json()['result']
-    # print(data)
-    lib.create_balance_table(data)
-    print('\n')
+    def private(self, method: str, params: Optional[Dict[str, Any]] = None):
+        return self._request("POST", f"/0/private/{method}", params, private=True)
 
-# get trade balances
-def get_trade_balance(api_key, api_sec):
-    """_summary_
+    # ------------------------------------------------------------------
+    # Dynamic endpoint access
+    # ------------------------------------------------------------------
+    def __getattr__(self, name: str):
+        if name in PUBLIC_METHODS:
+            return functools.partial(self.public, name)
+        if name in PRIVATE_METHODS:
+            return functools.partial(self.private, name)
+        raise AttributeError(name)
 
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
+
+# ----------------------------------------------------------------------
+# Convenience wrapper functions used by the CLI modules
+# ----------------------------------------------------------------------
+
+def ticker(pair: str = "XMREUR"):
+    api = KrakenAPI()
+    return api.Ticker({"pair": pair})
+
+
+def get_acc_balance(api_key: str, api_sec: str):
+    api = KrakenAPI(api_key, api_sec)
+    result = api.Balance()
+    lib.create_balance_table(result.get("result", {}))
+    return result
+
+
+def get_trade_balance(api_key: str, api_sec: str):
+    api = KrakenAPI(api_key, api_sec)
     asset = input("Enter Währung: ")
-    resp = kraken_request('/0/private/TradeBalance', {
-        "nonce": str(int(1000*time.time())), # Required
-        "asset": asset
-    }, api_key, api_sec, api_url)
-    # print(resp.json())
-    data = resp.json()['result']
-    lib.create_balance_table(data)
-    print('\n')
-
-# get open orders <----- Achtung!
-def get_open_orders(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/OpenOrders', {
-        "nonce": str(int(1000*time.time())), # Required
-        "trades": True
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# get closed orders <---- Achtung!
-def get_closed_orders(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/ClosedOrders', {
-        "nonce": str(int(1000*time.time())), # Required
-        "trades": '',
-        "userref": 36493663,
-        "start": '',
-        "end": '',
-        "ofs":'',
-        "closetime": ''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-
-# Query Orders Info <---- Achtung!
-def query_orders_info(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/QueryOrders', {
-        "nonce": str(int(1000*time.time())), # Required
-        "trades": '',
-        "userref": '',
-        "txid": "OBCMZD-JIEE7-77TH3F,OMMDB2-FSB6Z-7W3HPO",
-        "consolidate_take": ''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Get Trades History <---- Achtung!
-def get_trades_history(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/TradesHistory', {
-        "nonce": str(int(1000*time.time())), # Required
-        "type": '',
-        "trades": '',
-        "start": '',
-        "end": '',
-        "ofs":'',
-        "consolidate_take": ''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Query Trade Info <---- Achtung!
-def query_trades_info(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/QueryTrades', {
-        "nonce": str(int(1000*time.time())), # Required
-        "txid": "", # REQUIRED
-        "trades": True
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Get Open Positions <---- Achtung!
-def get_open_positions(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/OpenPositions', {
-        "nonce": str(int(1000*time.time())), # Required
-        "trades": '',
-        "userref": '',
-        "start": '',
-        "end": '',
-        "ofs":'',
-        "closetime": ''
-}, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Get Ledgers Info <---- Achtung!
-def get_ledgers_info(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/Ledgers', {
-        "nonce": str(int(1000*time.time())), # Required
-        "asset": '',
-        "aclass": '',
-        "type": '',
-        "start": '',
-        "end": '',
-        "ofs":''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Query Ledgers <---- Achtung!
-def query_ledgers_info(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/QueryLedgers', {
-        "nonce": str(int(1000*time.time())), # Required
-        "id": '',
-        "trades": '',
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Get Trade Volume <---- Achtung!
-def get_trade_volume(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/TradeVolume', {
-        "nonce": str(int(1000*time.time())), # Required
-        "pair": ''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Request Export Report <---- Achtung!
-def request_export_report(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/AddExport', {
-        "nonce": str(int(1000*time.time())), # Required
-        "report": '', # Required
-        "format": '',
-        "description": '', # Required
-        "fields": '',
-        "starttm": '',
-        "endtm": ''
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Get Export Report Status <---- Achtung!
-def get_export_report_status(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/GetExportReportStatus', {
-        "nonce": str(int(1000*time.time())), # Required
-        "report": 'ledgers' # Required
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Retrieve Data Export <---- Achtung!
-def retrieve_data_export(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/RetrieveExport', {
-        "nonce": str(int(1000*time.time())), # Required
-        "id": '' # Required
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-# Delete Export Report <---- Achtung!
-def delete_export_report(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/RemoveExport', {
-        "nonce": str(int(1000*time.time())), # Required
-        "id": '', # Required
-        "type": '' # Required
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
-
-############################## user data #################################
-##########################################################################
-############################# user trading ###############################
-
-# Withdraw Fund <---- Achtung!
-def withdraw_fund(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    with open('config/api/withdraw_db.json', 'r') as f:
-        data = json.load(f)
-    id = input("Geben Sie die Bezeichnung ein: ")
-    selected_data = data[str(id)]
-    nonce = str(int(1000*time.time()))
-    print(selected_data['asset'])
-    print(selected_data['key'])
-    betrag = input("Geben Sie die Höhe des Betrags ein: ")
-    resp = kraken_request('/0/private/Withdraw', {
-    "nonce": nonce, 
-    "asset": selected_data['asset'],
-    "key": selected_data['key'],
-    "amount": betrag
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n') 
-
-# Add Order <---- Achtung!
-def add_order(api_key, api_sec):
-    """_summary_
-
-    Args:
-        api_key (_type_): _description_
-        api_sec (_type_): _description_
-    """
-    resp = kraken_request('/0/private/AddOrder', {
-    "nonce": str(int(1000*time.time())),
-    "ordertype": "limit",
-    "type": "buy",
-    "volume": 0,
-    "pair": "XBTUSD",
-    "price": 27500
-    }, api_key, api_sec, api_url)
-    print(resp.json())
-    print('\n')
+    result = api.TradeBalance({"asset": asset})
+    lib.create_balance_table(result.get("result", {}))
+    return result
 
